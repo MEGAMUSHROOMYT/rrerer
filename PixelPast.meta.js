fbbd14cb6684773e56e3e09d1feb1540ed7432dd
MEGAMUSHROOMYT <108215828+MEGAMUSHROOMYT@users.noreply.github.com>
2024-03-25 19:33:15 +0100

    Add files via upload

---
+++ PixelPast.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         PixelPast
+
+// @version      1.2.4
+// @description  PixelPast is a free and open source mod for the game "Tanki Online"
+// @author       sheezzmee
+
+// @match        https://*.test-eu.tankionline.com/browser-public/index.html?*
+// @match        https://tankionline.com/play*
+// @match        https://tankionline.com/ru/
+// @match        https://tankionline.com/en/
+
+// @icon         https://raw.githubusercontent.com/sheezzmee/PixelPast/main/assets/icon.png
+
+// @downloadURL  https://raw.githubusercontent.com/sheezzmee/PixelPast/main/PixelPast.user.js
+// @updateURL    https://raw.githubusercontent.com/sheezzmee/PixelPast/main/PixelPast.meta.js
+
+// @grant        GM.xmlHttpRequest
+// @grant        GM.addStyle
+// @grant        unsafeWindow
+
+// @run-at       document-start
+
+// @connect      githubusercontent.com
+
+// ==/UserScript==
---
+++ README.md
@@ -0,0 +1,25 @@
+# PixelPast
+
+**PixelPast** - бесплатный мод с открытым исходным кодом для игры "*Танки онлайн*", возвращающий **старые текстуры** (до релиза **1.100.0**).
+
+![default](https://raw.githubusercontent.com/sheezzmee/PixelPast/main/assets/default.png)
+![legacy](https://raw.githubusercontent.com/sheezzmee/PixelPast/main/assets/legacy.png)
+
+Discord: **sheezzmee**
+
+## Установка
+
+**1.** Установите [Tampermonkey](https://www.tampermonkey.net/)
+
+**2.** Установите [скрипт](https://raw.githubusercontent.com/sheezzmee/PixelPast/main/PixelPast.user.js)
+
+## Участвовали в разработке
+
+- **sabaka-babaka**
+- **runas.exe**
+- Rabotaem
+- Beyyoonddeath
+- tdsrse
+- hopzy
+- Surrealism
+- predator
